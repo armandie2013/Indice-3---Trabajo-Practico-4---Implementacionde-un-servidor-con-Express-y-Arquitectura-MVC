475f3cd1ded46a72bc7b20ca83fd2136312e6c25
= <=>
2025-02-20 16:43:25 -0300

    Modificado

--- server.mjs
+++ server.mjs
@@ -8,10 +8,13 @@ import {
 const app = express();
 const PORT = 3005;
 
+// BUSCA POR ID //
 app.get("/superheroes/id/:id", obtenerSuperheroePorIdController);
 
+// BUSCA POR ATRIBUTO //
 app.get("/superheroes/atributo/:atributo/:valor", buscarSuperheroesPorAtributoController);
 
+// BUSCA POR EDAD //
 app.get("/superheroes/edad/mayorA30", obtenerSuperheroesMayoresDe30Controller);
 
 app.listen(PORT, () => {
